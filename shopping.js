4b38dfd6b354b0d53ccea9e702c9d2eb6e3cc10f
kahneraja <kahneraja@gmail.com>
2016-09-08 14:02:51 -0400

    edit

--- shopping.js
+++ shopping.js
@@ -1,63 +1,90 @@
-var Shopping = function (){
+"use strict";
+module.exports = function (){
 
-   var self = this;
+  this.parseShoppingData = function (input){
+    const lines = input.split("\n");
+    const overview = lines[0].split(" ");
+    const numberOfCentres = parseInt(overview[0]);
+    const numberOfRoads = parseInt(overview[1]);
+    const numberOfTypesOfFish = parseInt(overview[2]);
+    const targetCentreId = numberOfCentres;
 
-   self.distanceTo = function (id){
-     if (this.id == id)
-       return 0;
-     else {
-       var distance = 0;
-       for(var x = 0; x < this.roads.length; x++){
-         var road = this.roads[x];
-         console.log(road);
-         distance = road.distance + road.end.distanceTo(id);
-       }
+    let district = {
+      centres: []
+    };
 
-       console.log('distance ' + this.id + ' to ' + id + ' : ' + distance);
-       return distance;
-     }
-   };
+    for (let i = 0; i < numberOfCentres; i++) {
+      let products = lines[i + 1].split(" ");
+      products.shift();
+      let centre = {
+        id: i + 1,
+        products: products,
+        roads: [],
+        distanceTo: this.distanceTo,
+        isVisited: false
+      };
+      district.centres.push(centre);
+    }
 
-   self.parseShoppingData = function (input){
-     var lines = input.split('\n');
-     var overview = lines[0].split(' ');
-     var numberOfCentres = parseInt(overview[0]);
-     var numberOfRoads = parseInt(overview[1]);
-     var numberOfTypesOfFish = parseInt(overview[2]);
-     var targetCentreId = numberOfCentres;
+    for (let i = 0; i < numberOfRoads; i++) {
+      let roadSummary = lines[numberOfCentres + i + 1].split(" ");
+      let startIndex = parseInt(roadSummary[0]) - 1;
+      let endIndex = parseInt(roadSummary[1]) - 1;
+      let distance = parseInt(roadSummary[2]);
+      let departure = {
+        end: endIndex,
+        distance: distance
+      };
+      let arrival = {
+        end: startIndex,
+        distance: distance
+      };
+      district.centres[startIndex].roads.push(departure);
+      district.centres[endIndex].roads.push(arrival);
+    }
+    return district;
+  };
 
-     var district = {
-       centres: []
-     };
+  this.visitAll = function(district, start){
+    if (start.isVisited)
+      return 0;
+    start.isVisited = true;
+    let road = start.roads[0];
+    let destination = district.centres[road.end];
+    let distance = road.distance + this.visitAll(district, destination);
+    return distance;
+  };
 
-     for (var i = 0; i < numberOfCentres; i++) {
-       var products = lines[i + 1].split(' ');
-       products.shift();
-       var centre = {
-         id: i,
-         products: products,
-         roads: [],
-         distanceTo: self.distanceTo
-       };
-       district.centres.push(centre);
-     }
+  this.graph = function(district, centre, distance, history){
+    let nodes = [];
 
-     for (var i = 0; i < numberOfRoads; i++) {
-       var roadSummary = lines[numberOfCentres + i + 1].split(' ');
-       var startIndex = parseInt(roadSummary[0]);
-       var endIndex = parseInt(roadSummary[1]);
-       var distance = parseInt(roadSummary[2]);
+    for(let i = 0; i < centre.roads.length; i++){
+      let road = centre.roads[i];
+      let nextCentre = district.centres[road.end];
 
-       var road = {
-         start: district.centres[startIndex],
-         end: district.centres[endIndex],
-         distance: distance
-       }
-       district.centres[startIndex - 1].roads.push(road);
-     }
-     console.log(district);
-     return district;
-   }
+      let isVisited = false;
+      for(let i = 0; i < history.length; i++){
+        if (history[i] === nextCentre.id)
+          isVisited = true;
+      }
+
+      if (!isVisited){
+        let nextDistance = distance + road.distance;
+        let node = {
+          id: nextCentre.id,
+          distance: nextDistance,
+          nodes: this.graph(district, nextCentre, nextDistance, history.concat(centre.id))
+        };
+        nodes.push(node);
+      }
+    }
+    return nodes;
+  };
+
+  this.print = function(node, indentation){
+    console.log(Array(indentation * 2).join(" ") + node.id + ": " + node.distance);
+    for(let i = 0; i < node.nodes.length; i++){
+      this.print(node.nodes[i], indentation + 1);
+    }
+  };
 };
-
-module.exports = Shopping;
--- shopping.spec.js
+++ shopping.spec.js
@@ -1,12 +1,10 @@
-var assert = require('assert');
-var Shopping = require('./shopping.js');
-var shopping = new Shopping();
+const assert = require("assert");
+const Shopping = require("./shopping.js");
 
-
-describe('Shortest path to all fish and home.', function() {
-  it('Should parse centres', function() {
-    var shopping = new Shopping();
-    var input = `5 5 5
+describe("Shortest path to all fish and home.", function() {
+  it("Should parse centres", function() {
+    const shopping = new Shopping();
+    const input = `5 5 5
 1 1
 4 15 23 3 455
 1 3
@@ -19,31 +17,90 @@ describe('Shortest path to all fish and home.', function() {
 2 4 10
 3 5 10
 4 5 10`;
-    var district = shopping.parseShoppingData(input);
+    const district = shopping.parseShoppingData(input);
     assert.equal(5, district.centres.length);
     assert.equal(4, district.centres[1].products.length);
     assert.equal(4, district.centres[0].roads.length);
 
   });
 
-  it('Should find fastest route for one person in 2 minutes', function() {
-    var shopping = new Shopping();
-    var input = `5 5 5
+  it("Cities should have correct number of roads.", function() {
+    const shopping = new Shopping();
+    const input = `5 6 5
 1 1
-4 15 23 3 455
+1 2
 1 3
 1 4
 1 5
 1 2 10
 1 3 10
-1 4 10
+2 4 10
+3 5 10
+4 5 10
+5 1 10`;
+    const district = shopping.parseShoppingData(input);
+    assert.equal(3, district.centres[0].roads.length);
+  });
+
+  it("First city should have a road to 1,2.", function() {
+    const shopping = new Shopping();
+    const input = `5 6 5
+1 1
+1 2
+1 3
+1 4
+1 5
+1 2 10
+1 3 10
+2 4 10
+3 5 10
+4 5 10
+5 1 10`;
+    const district = shopping.parseShoppingData(input);
+    assert.equal(1, district.centres[0].roads[0].end);
+    assert.equal(2, district.centres[0].roads[1].end);
+  });
+
+  it("Default distance to 5 should be 40.", function() {
+    const shopping = new Shopping();
+    const input = `5 5 5
+1 1
+1 2
+1 3
+1 4
+1 5
+1 2 10
+1 3 10
 2 4 10
 3 5 10
 4 5 10`;
-    var district = shopping.parseShoppingData(input);
-    assert.equal(5, district.centres.length);
-    assert.equal(4, district.centres[1].products.length);
-    assert.equal(3, district.centres[0].roads.length);
-    assert.equal(2, district.centres[0].distanceTo(5));
+    const district = shopping.parseShoppingData(input);
+    const distance = shopping.visitAll(district, district.centres[0]);
+    assert.equal(20, distance);
   });
+
+  it("Should graph.", function() {
+    const shopping = new Shopping();
+    const input = `5 5 5
+1 1
+1 2
+1 3
+1 4
+1 5
+1 2 10
+1 3 10
+2 4 10
+3 5 10
+4 5 10`;
+    const district = shopping.parseShoppingData(input);
+    const start = district.centres[0];
+    const node = {
+      id: start.id,
+      distance: 0,
+      nodes: []
+    };
+    node.nodes = shopping.graph(district, district.centres[0], 0, []);
+    shopping.print(node,0);
+  });
+
 });
